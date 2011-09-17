c4f281a54ec441279c4d48478c61dff3b091c435
Kevin Su <ksu006@gmail.com>
2011-09-17 19:45:30 -0400

    Split chosen into dept and course.

--- front-end/static/js/base.js
+++ front-end/static/js/base.js
@@ -1,1 +1,11 @@
-//Hi
+
+// On ready
+$(document).ready(function() {
+    $(".chzn-select").chosen();
+    $("#search-dept").change(function() {
+	$.get("http://localhost:8000/options.html", { dept: $(this).val() },
+	      function(data) {
+		  $("#search-course").append(data).show().chosen();
+	      });
+    });
+});
