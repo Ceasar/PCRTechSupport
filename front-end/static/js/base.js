43336f2ee9676682e6b00af043a17f3b4147b630
Chris Chang <ccubed510@gmail.com>
2011-09-17 21:36:49 -0400

    Added lightbox to class information

--- front-end/static/js/base.js
+++ front-end/static/js/base.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 	var url = "http://localhost:8000/course/" + ($(this).val()).replace(" ", "/");
 	$.get(url,
 	      function(data) {
-		  $("#classInfo").html(data);
+		  $.colorbox({html: data});
 	      });
     });
 });
