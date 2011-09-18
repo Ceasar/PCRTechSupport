2256fb0636a8c0e76f97cd0b8122835aff2f52bd
Chris Chang <ccubed510@gmail.com>
2011-09-18 02:56:16 -0400

    Button functionality on course.html

--- front-end/static/js/base.js
+++ front-end/static/js/base.js
@@ -1,6 +1,7 @@
 
 // On ready
 $(document).ready(function() {
+    var urlCourse = " ";
     $(".chzn-select").chosen();
     $("#search-dept").change(function() {
 	$.get("http://localhost:8000/options", { dept: $(this).val() },
@@ -12,10 +13,14 @@ $(document).ready(function() {
     });
     $("#search-course").change(function() {
 	$("div#classInfo").html('<img src="http://www.seas.upenn.edu/~kevinsu/spiral.gif" height="200" />');
+	urlCourse = ($(this).val()).replace(" ", "/") + "/";
 	var url = "http://localhost:8000/course/" + ($(this).val()).replace(" ", "/") + "/";
 	$.get(url,
 	      function(data) {
 		  $.colorbox({html: data});
 	      });
     });
+    $('#
+
+
 });
