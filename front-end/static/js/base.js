040796a0a40b5e844f36e3564dd66c9c578d8877
Kevin Su <ksu006@gmail.com>
2011-09-18 04:45:23 -0400

    try to hide second chosen field

--- front-end/static/js/base.js
+++ front-end/static/js/base.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
     
     $(".chzn-select").chosen();
     $("#search-dept").change(function() {
+	$("#search-course_chzn").remove();
+	$("#search-course").removeClass("chzn-done").hide();
 	$.get("http://localhost:8000/options", { dept: $(this).val() },
 	      function(data) {
-		  $("#search-course_chzn").remove();
-		  $("#search-course").removeClass("chzn-done");
 		  $("#search-course").html(data).show().chosen();
 	      });
     });
